48c34b2ffd61b7cc4b7f420a90ae96dcd928f6c0
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-07-25 11:29:16 +0530

    Styles applied to the home page, Added background images

--- technomics/technomics/views.py
+++ technomics/technomics/views.py
@@ -2,12 +2,20 @@ import os
 
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.db import models
 
-
-def home(request):
-    
-    return TemplateView.as_view(template_name='home.html')
+from models import Contactus, Dates, Homepage, Feature, Newsevents, Aboutus, Blog, Comment, Slideshow, Services, Services_section, Testimonials
 
 # def home(request):
     
 #     return TemplateView.as_view(template_name='home.html')
+
+class Homepage_view():
+
+
+
+
