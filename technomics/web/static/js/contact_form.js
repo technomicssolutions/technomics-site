d0a13a3f72bb195f79af290ba2149095c72e9925
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-08-01 11:01:14 +0530

    Changed the styles, automatic mail to the admins

--- technomics/web/static/js/contact_form.js
+++ technomics/web/static/js/contact_form.js
@@ -12,21 +12,16 @@ window.addEvent('domready', function(){
     $('send').addEvent('click', function(e){
         e.stop();
         var contact_data = $('contact_form').toQueryString();
-        console.log(contact_data);
         var ajaxRequest = new Request({ 
             url: '/contactus',
             method: 'POST',
             data: contact_data,
-            onRequest: function(){
-              alert('just sent a Ajax request!');
-            },
             onComplete: function(){
-                alert('completed');
+                alert('You have successfully sent the Message');
             }
         }); 
         ajaxRequest.send(); 
-	});
-    
+	});  
 });
 
 
