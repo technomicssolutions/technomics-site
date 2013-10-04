2fb5a7a8e03caf0b5ac17840ab53bd684e0e1737
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-10-04 21:28:34 +0530

    Templatetags added

---
+++ technomics/web/templatetags/base_tags.py
@@ -0,0 +1,17 @@
+import re
+
+from django import template
+register = template.Library()
+
+
+@register.filter        
+def truncatechars(value, limit):
+    try:
+        limit = int(limit)
+    except ValueError: # invalid literal for int()
+        return value # Fail silently. 
+    if len(value) <= limit:
+        truncated_value = value
+    else:
+        truncated_value = value[0:limit-3]
+    return truncated_value
