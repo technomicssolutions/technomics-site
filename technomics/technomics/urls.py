4cd52208f48fe0a7335ad8dfa29faf1835b8523f
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-10-05 20:59:07 +0530

    Changes in url

--- technomics/technomics/urls.py
+++ technomics/technomics/urls.py
@@ -8,12 +8,12 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/', include(admin.site.urls)),
     url(r'^$',views.home, name="home"),
     url(r'^contactus$',views.contact_us),
     url(r'^(?P<slug>[-\w]+)/$', views.renderpage, name = "render_page"),
     url(r'^contact_notification$', TemplateView.as_view(template_name='contactus_notification.html')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-	url(r'^admin/', include(admin.site.urls)),
 	url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
    url(r'^static1/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.GENERATED_MEDIA_ROOT, 'show_indexes': True}),
 )
