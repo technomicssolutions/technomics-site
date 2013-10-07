5b510cc9f92ce135dc1f5c1bcf88ee84c3f83368
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-10-07 23:46:25 +0530

    Submenu page creation

---
+++ technomics/web/migrations/0011_auto__del_field_aboutus_right_content__del_field_aboutus_left_content.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Aboutus.right_content'
+        db.delete_column(u'web_aboutus', 'right_content')
+
+        # Deleting field 'Aboutus.left_content'
+        db.delete_column(u'web_aboutus', 'left_content')
+
+    def backwards(self, orm):
+        # Adding field 'Aboutus.right_content'
+        db.add_column(u'web_aboutus', 'right_content',
+                      self.gf('django.db.models.fields.TextField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Aboutus.left_content'
+        db.add_column(u'web_aboutus', 'left_content',
+                      self.gf('django.db.models.fields.TextField')(null=True, blank=True),
+                      keep_default=False)
+
+    models = {
+        u'web.aboutus': {
+            'Meta': {'object_name': 'Aboutus', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.blog': {
+            'Meta': {'object_name': 'Blog', '_ormbases': [u'web.Dates']},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.comment': {
+            'Meta': {'object_name': 'Comment', '_ormbases': [u'web.Dates']},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'blog_id': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['web.Blog']"}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'web.contactus': {
+            'Meta': {'object_name': 'Contactus', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'email_id': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'message': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80'})
+        },
+        u'web.dates': {
+            'Meta': {'object_name': 'Dates'},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.feature': {
+            'Meta': {'object_name': 'Feature', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.homepage': {
+            'Meta': {'object_name': 'Homepage', '_ormbases': [u'web.Dates']},
+            'customer_care': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'web.menu': {
+            'Meta': {'object_name': 'Menu', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': "'1'", 'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.newsevents': {
+            'Meta': {'object_name': 'Newsevents', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'event_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.services': {
+            'Meta': {'object_name': 'Services', '_ormbases': [u'web.Dates']},
+            'banner_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'content_subhead': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'web.services_section': {
+            'Meta': {'object_name': 'Services_section', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'section_head': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'section_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'web.slide': {
+            'Meta': {'object_name': 'Slide', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'slideshow_id': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['web.Slideshow']"}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.slideshow': {
+            'Meta': {'object_name': 'Slideshow', '_ormbases': [u'web.Dates']},
+            'bullet_active': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'bullet_inactive': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'left_arrow': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'max_slide_count': ('django.db.models.fields.IntegerField', [], {'default': '3'}),
+            'right_arrow': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'web.submenu': {
+            'Meta': {'object_name': 'Submenu', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'menu': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['web.Menu']"}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': "'1'", 'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.testimonials': {
+            'Meta': {'object_name': 'Testimonials', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['web']
---
+++ technomics/web/migrations/0012_auto__add_field_contactus_subject.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Contactus.subject'
+        db.add_column(u'web_contactus', 'subject',
+                      self.gf('django.db.models.fields.CharField')(default='test', max_length=100),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'Contactus.subject'
+        db.delete_column(u'web_contactus', 'subject')
+
+    models = {
+        u'web.aboutus': {
+            'Meta': {'object_name': 'Aboutus', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.blog': {
+            'Meta': {'object_name': 'Blog', '_ormbases': [u'web.Dates']},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.comment': {
+            'Meta': {'object_name': 'Comment', '_ormbases': [u'web.Dates']},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'blog_id': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['web.Blog']"}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'web.contactus': {
+            'Meta': {'object_name': 'Contactus', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'email_id': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'message': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'web.dates': {
+            'Meta': {'object_name': 'Dates'},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.feature': {
+            'Meta': {'object_name': 'Feature', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.homepage': {
+            'Meta': {'object_name': 'Homepage', '_ormbases': [u'web.Dates']},
+            'customer_care': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'web.menu': {
+            'Meta': {'object_name': 'Menu', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': "'1'", 'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.newsevents': {
+            'Meta': {'object_name': 'Newsevents', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'event_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.services': {
+            'Meta': {'object_name': 'Services', '_ormbases': [u'web.Dates']},
+            'banner_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'content_subhead': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'web.services_section': {
+            'Meta': {'object_name': 'Services_section', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'section_head': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'section_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'web.slide': {
+            'Meta': {'object_name': 'Slide', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'slideshow_id': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['web.Slideshow']"}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.slideshow': {
+            'Meta': {'object_name': 'Slideshow', '_ormbases': [u'web.Dates']},
+            'bullet_active': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'bullet_inactive': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'left_arrow': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'max_slide_count': ('django.db.models.fields.IntegerField', [], {'default': '3'}),
+            'right_arrow': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'web.submenu': {
+            'Meta': {'object_name': 'Submenu', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'menu': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['web.Menu']"}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': "'1'", 'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'web.testimonials': {
+            'Meta': {'object_name': 'Testimonials', '_ormbases': [u'web.Dates']},
+            u'dates_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['web.Dates']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['web']
