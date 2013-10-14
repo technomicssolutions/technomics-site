5c320ff4ca0b766fcf8715ecf0d700f7c2bf04f9
technomics <technomics@technomics-desktop.(none)>
2013-10-14 16:10:01 +0530

    comment posting done via ajax

--- technomics/web/static/js/add_comment.js
+++ technomics/web/static/js/add_comment.js
@@ -3,22 +3,21 @@ window.addEvent('domready', function(){
     if (send_comment) {
         send_comment.addEvent('click', function(e){
              e.stop();
-    //         alert('hai')
-             var contact_data = $('comment_form').toQueryString();
-              console.log('data'+contact_data);
-     //        var ajaxRequest = new Request({ 
-     //            url: '/contactus',
-     //            method: 'POST',
-     //            data: contact_data,
-     //            onSuccess: function(){
-     //                console.log('You have successfully sent the Message');
-     //            }
-     //            // onFailure: function(){
-     //            //     console.log('error :'+ data);
-     //            // }
+             var comment_data = $('comment_form').toQueryString();
+             var blog_id = $('hidden_blog_id').get('name');
+             var ajaxRequest = new Request({ 
+                 url: '/blog/comment/add/'+blog_id+'/',
+                 method: 'POST',
+                 data: comment_data,
+                 onSuccess: function(){
+                     console.log('You have successfully sent the Message');
+                 }
+//                  onFailure: function(){
+//                      console.log('error :'+ data);
+//                  }
 
-     //        }); 
-     //        ajaxRequest.send(); 
+             }); 
+             ajaxRequest.send(); 
 	     }); 
         // $('submit_form').addEvent('click', function(e){
         //     e.stop();
