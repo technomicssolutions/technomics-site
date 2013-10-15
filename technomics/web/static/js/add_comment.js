99765c25963175b8bf9d3c6eee86301531a40656
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-10-15 17:48:30 +0530

    comment form js creation

--- technomics/web/static/js/add_comment.js
+++ technomics/web/static/js/add_comment.js
@@ -1,16 +1,22 @@
 window.addEvent('domready', function(){
-    
-    var blog_comment_links = $$('.blog_comment');
-    var comment_form = $('comment_form');
-    blog_comment_links.each(function(item, index){
-        item.addEvent('click', function(e) {
-        e.stop();
-        if (item.getParent().contains(comment_form)) {
-            ;
-        }
-        else {
-            comment_form.inject(item.getParent(), 'after');
-        }
-        });
+    $$('.form_comment').hide();
+    // var blog_comment_links = $$('.blog_comment');
+    // var comment_form = $('comment_form');
+    // blog_comment_links.each(function(item, index){
+    //     item.addEvent('click', function(e) {
+    //     e.stop();
+    //     if (item.getParent().contains(comment_form)) {
+    //         ;
+    //     }
+    //     else {
+    //         comment_form.inject(item.getParent(), 'after');
+    //     }
+    // });
+    $$('.blog_comment').addEvent('click', function(e){
+        $$('.blog_comment').hide();
+        form_id = this.getParent().getElement('form').get('id');
+        $(form_id).show();
+    });
 });
 
+// {% url 'add_blog_comment' blog_id=blog.id %}?page={{ blogs.number }}
