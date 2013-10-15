ae3a28eb2cb042e77f3d59adea92338c4d14989a
Geethu <geethu@geethu-Lenovo-G570.(none)>
2013-10-15 23:38:34 +0530

    Comment js file creation

--- technomics/web/static/js/add_comment.js
+++ technomics/web/static/js/add_comment.js
@@ -17,6 +17,22 @@ window.addEvent('domready', function(){
         form_id = this.getParent().getElement('form').get('id');
         $(form_id).show();
     });
+    $$('.comment_submit').addEvent('click', function(e) {
+        $$('.blog_comment').show();
+        // form_id = this.getParent().get('id');
+        // var form_data = $(form_id).toQueryString();
+        // var url = 'blog/comment/add/'+form_id+'/';
+        // var ajaxRequest = new Request({ 
+        //     url: url,
+        //     method: 'POST',
+        //     data: form_data,
+        //     onSuccess: function(){
+        //         console.log('You have successfully sent the Message');
+        //     }
+        // });
+        $(form_id).hide();
+
+    });
 });
 
 // {% url 'add_blog_comment' blog_id=blog.id %}?page={{ blogs.number }}
